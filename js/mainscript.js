4263f73ca61d323f64b4d1cd3cb5b712c65a456d
Tuomas Tammela <tuomas.t@workboost.eu>
2019-04-13 01:36:06 +0300

    Using metadata to get full station names from short codes

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,46 +1,84 @@
-function fetchTrainSchedules() {
+var stationNamesList = [];
 
-	var url = "https://rata.digitraffic.fi/api/v1/live-trains/station/";
+function initStationList() {
 
-	var station = "HKI";
-	
-	url += station;
-
-	var jsonObject;
+	var url = "https://rata.digitraffic.fi/api/v1/metadata/stations";
+	var stationsData;
 
 	$.ajax({
 		type: "GET",
 		dataType: "json",
 		url: url,
 		data: "",
-		success: function (jsonObject) {
-			createPage(jsonObject);
+		success: function (stationsData) {
+			
+			stationNamesList = stationsData.map(function(station) {
+				return {
+					name: station.stationName,
+					shortCode: station.stationShortCode
+				};
+			});
+			
+			$.each(stationNamesList, function(i, station) {
+				$("#stationSelect").append("<option value='" + station.name + "'>");
+			});
+		},
+		error: function() {
+			alert("Error: unable to retrieve station data");
+		}
+	});
+}
+
+initStationList();
+
+function fetchTrainSchedules() {
+
+	var stationName = $("#stationInput").val();
+	var stationCode = stationNamesList.find(value => value.name == stationName).shortCode;
+	
+	var url = "https://rata.digitraffic.fi/api/v1/live-trains/station/" + stationCode;
+	var trainsData;
+
+	$.ajax({
+		type: "GET",
+		dataType: "json",
+		url: url,
+		data: "",
+		success: function (trainsData) {
+			createPage(trainsData);
 		},
 		error: function() {
 			alert("Error: unable to retrieve train data");
 		}
-	})
+	});
 }
 
-function createPage(jsonObject) {
+function createPage(trainsData) {
 
 	$("#trainRows").empty();
 
-	for (var i = 0; i < jsonObject.length; i++) {
+	for (var i = 0; i < trainsData.length; i++) {
 
 		var tableRow = $("<tr>");
 	
-		if (jsonObject[i].trainNumber != null) {
-			tableRow.append($("<td>").text(jsonObject[i].trainNumber));
+		if (trainsData[i].trainNumber != null) {
+			tableRow.append($("<td>").text(trainsData[i].trainNumber));
 		}
 		else {
 			tableRow.append($("<td>").text("N/A"));
 		}
 		
-		if (jsonObject[i].timeTableRows != null) {
-			tableRow.append($("<td>").text(jsonObject[i].timeTableRows[0].stationShortCode));
-			tableRow.append($("<td>").text(jsonObject[i].timeTableRows[jsonObject[i].timeTableRows.length-1].stationShortCode));
-			tableRow.append($("<td>").text(new Date(jsonObject[i].timeTableRows[jsonObject[i].timeTableRows.length-1].scheduledTime).toLocaleTimeString()));
+		if (trainsData[i].timeTableRows != null) {
+
+			var departureStationCode = trainsData[i].timeTableRows[0].stationShortCode;
+			var departureStationName = stationNamesList.find(value => value.shortCode == departureStationCode).name.replace(" asema","");
+			tableRow.append($("<td>").text(departureStationName));
+			
+			var arrivalStationCode = trainsData[i].timeTableRows[trainsData[i].timeTableRows.length-1].stationShortCode;
+			var arrivalStationName = stationNamesList.find(value => value.shortCode == arrivalStationCode).name.replace(" asema","");
+			tableRow.append($("<td>").text(arrivalStationName));
+			
+			tableRow.append($("<td>").text(new Date(trainsData[i].timeTableRows[trainsData[i].timeTableRows.length-1].scheduledTime).toLocaleTimeString()));
 		}
 		else {
 			tableRow.append($("<td>").text("N/A"));
@@ -52,4 +90,11 @@ function createPage(jsonObject) {
 	}
 }
 
-$("#fetch").on("click", fetchTrainSchedules);
+$(document).ready(function() {
+	$('#stationInput').keydown(function(event) {
+		if (event.which == 13) {
+			fetchTrainSchedules();
+			event.preventDefault();
+		}
+	});
+});
